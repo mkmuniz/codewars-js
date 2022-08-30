64f9c176083d4235372168d008f56b34d8a47c3b
worstp <mikaelmuniz2001@gmail.com>
2022-08-30 20:04:18 -0300

    first commit

---
+++ 7kyu/RemoveAllMarkedElements.js
@@ -0,0 +1,1 @@
+Array.prototype.remove_ = (integer_list, values_list) => integer_list.filter(num => !values_list.includes(num))
---
+++ 8kyu/WilsonPrime.js
@@ -0,0 +1,5 @@
+function amIWilson(p) {
+    if (p === 5 || p === 13 || p === 563) return true;
+
+    return false;
+}
